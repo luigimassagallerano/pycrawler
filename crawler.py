482d66ccc8adb58ea6a09ba8655dfb4ec60daea1
Luigi Massa Gallerano <luigimassagallerano@gmail.com>
2016-11-13 18:51:20 +0100

    Make the crawler crawl randomly
    At the moment it just jumps randomly to one of the downloaded URLs
    and start again.
    
    The case where the list of URLs is empty is not yet handled.
    Also loops are not yet handled.

--- crawler.py
+++ crawler.py
@@ -1,14 +1,20 @@
 import urllib2
 import re
+import random
 from bs4 import BeautifulSoup
 
 url = "http://www-rohan.sdsu.edu/~gawron/index.html"
 
-html = urllib2.urlopen(url).read()
-soup = BeautifulSoup(html)
-links = soup.findAll('a', attrs={'href': re.compile("^http://")})
+while url is not None:
+    print url
 
-for tag in links:
-    link = tag.get('href', None)
-    if link is not None and link:
-        print link
+    try:
+        html = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(html)
+        links = soup.findAll('a', attrs={'href': re.compile("^http://")})
+        tag = random.choice(links)
+        link = tag.get('href', None)
+        url = link
+    except Exception:
+        url = random.choice(links).get('href', None)
+
